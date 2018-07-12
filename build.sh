1f43ffcb18773b8425e4b686f70d35ba2f4dc96d
Jan Galinski <jan.galinski@holisticon.de>
2018-07-12 22:37:35 +0200

    config cache

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ set -e
 
 ./gradlew -b bom/build.gradle.kts build publishToMavenLocal
 
+ls -lR $HOME/.m2/repository/io/holunda
+
 ./gradlew build
 ./gradlew publishToMavenLocal
 mvn clean verify -f examples/maven-java-example
