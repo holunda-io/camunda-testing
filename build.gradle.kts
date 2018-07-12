6f4032905d66bdf05582ef798bb15935e82109b0
Jan Galinski <jan.galinski@holisticon.de>
2018-07-12 14:49:46 +0200

    add java plugin and version 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,18 @@ plugins {
   base
   kotlin("jvm") version "1.2.51" apply false
   idea
+  java
 }
 
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
 
 
 allprojects {
 
   group = "io.holunda.testing"
-
   version = "0.0.1-SNAPSHOT"
 
   repositories {
